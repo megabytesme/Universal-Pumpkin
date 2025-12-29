7aacd3445b466c92a935d44b1f1556e43ffef925
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-12-29 18:38:16 +0000

    Client - Feature: Added new UWP Client (10240, 1507 target)

---
+++ client/Universal Pumpkin (1507)/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Universal_Pumpkin
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NavListBox.SelectedIndex = 0;
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            RootSplitView.IsPaneOpen = !RootSplitView.IsPaneOpen;
+        }
+
+        private void NavListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NavListBox.SelectedItem is ListBoxItem item)
+            {
+                string tag = item.Tag.ToString();
+                switch (tag)
+                {
+                    case "Console":
+                        ContentFrame.Navigate(typeof(ConsolePage));
+                        break;
+                    case "Settings":
+                        ContentFrame.Navigate(typeof(SettingsPage));
+                        break;
+                }
+            }
+        }
+    }
+}
