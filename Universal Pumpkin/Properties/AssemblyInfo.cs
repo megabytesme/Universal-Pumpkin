93741126bfd0d674a4d649f2eb7213e5236a548a
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-12-30 04:15:23 +0000

    Client: Updated AssemblyInfo.cs

--- Universal Pumpkin/Properties/AssemblyInfo.cs
+++ Universal Pumpkin/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Universal Pumpkin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Empowering everyone to host fast and efficient Minecraft servers on Universal Windows Platform (UWP) supporting devices.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MegaBytesMe")]
 [assembly: AssemblyProduct("Universal Pumpkin")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright © 2025 MegaBytesMe")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: ComVisible(false)]
